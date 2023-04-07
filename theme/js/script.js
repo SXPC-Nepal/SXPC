428b1140a227104e85a19e585f92aa83a33b7e2b
SXPC-Nepal <022bscphy018@sxc.edu.np>
2023-04-07 11:10:09 +0545

    update

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,9 +1,4 @@
-/**
- * WEBSITE: https://themefisher.com
- * TWITTER: https://twitter.com/themefisher
- * FACEBOOK: https://www.facebook.com/themefisher
- * GITHUB: https://github.com/themefisher/
- */
+
 
 (function ($) {
   'use strict';
@@ -181,5 +176,37 @@
       $('.navigation').removeClass('sticky-header');
     }
   });
+
+// Set the date we're counting down to
+var countDownDate = new Date("Apr 12, 2023 15:00:00").getTime();
+
+// Update the countdown every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Calculate the distance between now and the countdown date
+  var distance = countDownDate - now;
+
+  // Calculate days, hours, minutes, and seconds left
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the countdown timer
+  document.querySelector(".days").innerHTML = days;
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML = minutes;
+  document.querySelector(".seconds").innerHTML = seconds;
+
+  // If the countdown is finished, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.querySelector(".timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
 
 })(jQuery);
